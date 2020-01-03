41798b999c20bd453ca6cd2dac4185f607977b01
DingXing <wildhunt_geralt@foxmail.com>
2020-01-03 19:13:00 +0800

    modify mvn group id

---
+++ slide-server/src/main/java/cn/org/nf404/slide/server/SliderServerConfiguration.java
@@ -0,0 +1,11 @@
+package cn.org.nf404.slide.server;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dx DingXing
+ * @date 2020-01-03
+ */
+@Configuration
+public class SliderServerConfiguration {
+}
---
+++ slide-web/src/main/java/cn/org/nf404/slide/web/SliderWebConfiguration.java
@@ -0,0 +1,13 @@
+package cn.org.nf404.slide.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author dx DingXing
+ * @date 2020-01-03
+ */
+@Configuration
+@EnableWebMvc
+public class SliderWebConfiguration {
+}
